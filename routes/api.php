5c640ae10e9ca9b6b0aa22e804088b8189165812
Victor Gustavo <victorgustavo.dev@gmail.com>
2025-07-23 17:09:51 -0300

    refactor: implementa o cors no config/

--- routes/api.php
+++ routes/api.php
@@ -5,19 +5,15 @@ use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\RequestController;
 use App\Http\Controllers\TypeReqsController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 
 Route::get('/', function () {
     return response()->json([
         'message' => 'API Laravel iniciada!',
     ]);
 });
+
+Route::post('/login', [UserController::class, 'login']);
 
 
 Route::apiResource('users', UserController::class);
